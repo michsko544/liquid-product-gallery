e6a71bf09a7507b5d64f086161f384d5bfc509a8
Michał Skorus <michal.skorus@nubisoft.pl>
2024-05-18 16:47:59 +0200

    Add product slider with bestselling products, add product card snippet

--- scripts/swiper.ts
+++ scripts/swiper.ts
@@ -7,4 +7,21 @@ import { Swiper } from 'swiper';
 new Swiper('.swiper', {
   direction: 'horizontal',
   loop: true,
+  slidesPerView: 'auto',
+  spaceBetween: 10,
+
+  breakpoints: {
+    600: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
 });
